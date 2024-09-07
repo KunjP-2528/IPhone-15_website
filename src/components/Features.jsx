593323a747f7d83a27af6530702a1bc57833ac21
KunjP-2528 <165396689+KunjP-2528@users.noreply.github.com>
2024-09-07 17:30:11 +0530

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import React, { useRef } from 'react'
 import { animateWithGsap } from "../utils/Animation";
-import { explore1Img, explore2Img, exploreVideo } from '../utils';
+import { explore1Img, explore2Img, exploreVideo } from '../utils/index';
 import gsap from 'gsap';
 
 const Features = () => {
